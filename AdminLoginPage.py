7c271ed7d2be4cf21660b38e20708e7d4eb0fa66
capnconn <coriche18@gmail.com>
2022-02-11 17:35:31 -0600

    Created AdminLoginPage ui and corresponding py files

---
+++ AdminLoginPage.py
@@ -0,0 +1,41 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit
+import sys
+
+QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+class UI(QMainWindow):
+	def __init__(self):
+		super(UI, self).__init__()
+
+		uic.loadUi("AdminLoginPage.ui", self)
+
+		# Define our widgets
+		self.usernameEdit = self.findChild(QLineEdit, "UsernameValue")
+		self.passwordEdit = self.findChild(QLineEdit, "PasswordValue")
+		self.loginButton = self.findChild(QPushButton, "LoginButton")
+		self.viewButton = self.findChild(QPushButton, "ViewButton")
+
+		# Set pointer when either buttons have been hovered over
+		self.loginButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+		self.viewButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+
+		# Handle submit click
+		self.loginButton.clicked.connect(self.handleSubmit)
+
+		self.show()
+
+	def handleSubmit(self):
+		print("Username: " + self.usernameEdit.text())
+		print("Password: " + self.passwordEdit.text())
+
+
+
+# Initialize the App
+app = QApplication(sys.argv)
+UIWindow = UI()
+
+app.exec_()
