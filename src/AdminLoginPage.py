76a03bcb9b584f8da02a2eea9ffb07d4cb98f931
capnconn <coriche18@gmail.com>
2022-02-11 23:44:34 -0600

    added some authentication to the login form

--- src/AdminLoginPage.py
+++ src/AdminLoginPage.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QCheckBox
 import sys, os
 
 # Go back to the previous directory - to access the ui folder
@@ -22,6 +22,19 @@ class UI(QMainWindow):
 		self.passwordEdit = self.findChild(QLineEdit, "PasswordValue")
 		self.loginButton = self.findChild(QPushButton, "LoginButton")
 		self.viewButton = self.findChild(QPushButton, "ViewButton")
+		self.incorrectField = self.findChild(QLabel, "IncorrectField")
+		self.fieldMissing = self.findChild(QLabel, "FieldMissing")
+		self.showPassword = self.findChild(QCheckBox, "ShowPassword")
+
+		# Connect a show password function
+		self.showPassword.stateChanged.connect(self.unhidePassword)
+
+		# Set password to be hidden
+		self.passwordEdit.setEchoMode(QLineEdit.Password)
+
+		# Hide the alert message --> only display it when necessarys
+		self.fieldMissing.setHidden(True)
+		self.incorrectField.setHidden(True)
 
 		# Set pointer when either buttons have been hovered over
 		self.loginButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
@@ -33,7 +46,37 @@ class UI(QMainWindow):
 
 		self.show()
 
+	# Hide/unhide password based on whether or note checkbox is checked
+	def unhidePassword(self):
+		if self.passwordEdit.echoMode() == QLineEdit.Normal:
+			self.passwordEdit.setEchoMode(QLineEdit.Password)
+		else:
+			self.passwordEdit.setEchoMode(QLineEdit.Normal)
+
 	def handleSubmit(self):
+
+		# Check if username and password field are filled out
+		if self.usernameEdit.text().strip() == '' and self.passwordEdit.text().strip() == '':
+			self.fieldMissing.setText("* Username and password field's are required.")
+			self.fieldMissing.setHidden(False)
+			return
+		elif self.usernameEdit.text().strip() == '':
+			self.fieldMissing.setText("* Username field is required.")	
+			self.fieldMissing.setHidden(False)
+			return
+		elif self.passwordEdit.text().strip() == '':
+			self.fieldMissing.setText("* Password field is required.")
+			self.fieldMissing.setHidden(False)
+			return
+		self.fieldMissing.setHidden(True)
+
+		# Check if credentials are correct
+		if not self.usernameEdit.text().strip() == 'Connor' and not self.passwordEdit.text().strip() == 'password':
+			self.incorrectField.setHidden(False)
+			return
+
+		self.incorrectField.setHidden(True)
+
 		print("Username: " + self.usernameEdit.text())
 		print("Password: " + self.passwordEdit.text())
 
