f2f1c2486c22d98a86bc61fd34ca6eadcb78da27
capnconn <coriche18@gmail.com>
2022-03-21 21:17:44 -0500

    made some changes leftover from merge

--- src/App.py
+++ src/App.py
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
 from dash import Dash, dcc, html, Input, Output, callback
-from pages import AdminLoginPage, AddNewBranch, MainMenu
-App = Dash(__name__, suppress_callback_exceptions=True)
-
-App.layout = html.Div([
-    dcc.Location(id='url', refresh=True),
-    html.Div(id='page-content')
-])
-
-
-@callback(
-    Output('page-content', 'children'),
-    Input('url', 'pathname'))
-def display_page(pathname):
-    if pathname == '/LoginPage':
-        return AdminLoginPage.layout
-    elif pathname == '/MainMenu':
-        return MainMenu.layout
-    elif pathname == '/AddNewBranch':
-        return AddNewBranch.layout
-    else:
-        return '404'
-
-
-if __name__ == '__main__':
-    App.run_server(debug=True)
-||||||| empty tree
-=======
-from dash import Dash, dcc, html, Input, Output, callback
-from pages import AdminLoginPage, AddNewBranch, DeletePage
+from pages import AdminLoginPage, AddNewBranch, DeletePage, MainMenu
 App = Dash(__name__, suppress_callback_exceptions=True)
 
 App.layout = html.Div([
@@ -55,4 +26,3 @@ def display_page(pathname):
 
 if __name__ == '__main__':
     App.run_server(debug=True)
->>>>>>> 5aca30f14c951e38e8c9dd4c304488e00dceb151
