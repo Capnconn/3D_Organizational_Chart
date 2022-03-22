10dc0d384ccb0c64f8d059c5c264c5b2243ad17d
capnconn <coriche18@gmail.com>
2022-03-21 20:04:05 -0500

    reverting to previous commit

---
+++ src/pages/MainMenu.py
@@ -0,0 +1,42 @@
+from dash import Dash, dcc, html, Input, Output, callback, State
+
+layout = html.Div(className='MainMenu', 
+	children=[
+		html.H1('Bayer 3D Chart', className='MainMenuTitle'),
+		html.Div(className='MainMenuButtons', 
+			children=[
+				html.P(id='spacing'),
+				html.Button('Add branch', id='addBranchButton', className='addBranchMenuCss'),
+				html.Button('Delete branch', id='deleteBranchButton', className='deleteBranchMenuCss'),
+				html.Button('Edit branch', id='editBranchButton', className='editBranchMenuCss'),
+				html.Button('Filter', id='filterButton', className='filterMenuCss'),
+				html.Div (className='square')
+			]
+			
+		),
+		
+		html.Div(id='redirect_add_branch'),
+		html.Div(id='redirect_delete_branch'),
+		#html.Div(id='redirect_edit_branch'),
+
+	]
+)
+
+@callback(
+	Output('redirect_add_branch', 'children'),
+	Input('addBranchButton', 'n_clicks'),
+	prevent_initial_call=True
+)
+def handleAddBranch(n_clicks):
+	return dcc.Location(pathname='/AddNewBranch', id='tempL')
+
+@callback(
+	Output('redirect_delete_branch', 'children'),
+	Input('deleteBranchButton', 'n_clicks'),
+	prevent_initial_call=True
+)
+
+def handleDeleteBranch(n_clicks):
+	return dcc.Location(pathname='/DeleteBranch', id='tempL')
+
+
