0101ee8e728a340aaaf64d781b5b0f8df3272b55
ashlenash <ashlenashton@yahoo.com>
2022-04-13 12:48:31 -0500

    Edited DeletePage.py to better query database. Updated Class identifiers in the css file.

--- src/pages/DeletePage.py
+++ src/pages/DeletePage.py
@@ -5,63 +5,57 @@
 #Date: 03-18-2022
 
 from dash import Dash, dcc, html, Input, Output, callback, State
+import mysql.connector
+
+connection = mysql.connector.connect(user='root', password='root', host='localhost', database='bayerdatabase')
+bayerdb = connection.cursor()
 
 layout = html.Div(className='DeletePageLayout',
     children=[
-        html.H1("Delete a Link or Node", className="deleteNodeLabel"),
-        html.Div(className="deleteLinkBody",
+        html.H1("Delete a branch from the network", className="deleteNodeLabel", style={'color':'white'}),
+		html.P("Enter a title for Node 1 to delete that organizational branch and all of its' connections from Bayer network. Enter titles for two nodes to delete the connection between those two nodes.", id="instructions", className="instructions"),
+		html.Br(),
+        html.Div(className="deleteNode1Body",
             children=[
-                html.P("Link: ", id="currentLink", className="linkLabel"),
-                dcc.Input(id="linkTitle", type="search", style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
+                html.P("Node 1: ", id="Node1", className="node1Label"),
+                dcc.Input(id="node1Title", type="search", style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
                 html.P(id='spacing'),
-                html.Button("Delete Link", id="deleteLinkButton",className="deleteALink")
             ],
         ),
         
-        html.Div(className="deleteNodeBody",
+        html.Div(className="deleteNode2Body",
             children=[
-                html.P("Node: ", id="currentNode", className="nodeLabel"),
-                dcc.Input(id="nodeTitle", type="search",  style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
+                html.P("Node 2: ", id="Node2", className="node2Label"),
+                dcc.Input(id="node2Title", type="search",  style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
                 html.P(id='spacing'),
-                html.Button("Delete Node", id="deleteNodeButton", className="deleteANode")
+                
             ],
             
         ),
         
+        html.Button("Delete Branch", id="deleteBranchButton", className="deleteABranch"),
         html.Button('Home', id='goHomeButton', className='goHome'),
-        html.Div(id="hidden_div_for_redirect_callback_delete_link"),
-        html.Div(id="hidden_div_for_redirect_callback-delete_node"),
+        html.Div(id="hidden_div_for_redirect_callback_delete_branch"),
         html.Div(id="hidden_div_for_redirect_callback_return_home")
     ]
 )
 
 @callback(
-    Output('hidden_div_for_redirect_callback_delete_link', 'children'),
-    Input('deleteLinkButton', 'n_clicks'),
-    State('linkTitle', 'value'),
+    Output('hidden_div_for_redirect_callback_delete_branch', 'children'),
+    Input('deleteBranchButton', 'n_clicks'),
+    State('node1Title', 'value'),
+	State('node2Title', 'value'),
     prevent_initial_call=True
 )
 
-def handleDeleteLink(n_clicks, link):
-    print((link))
-    if not link:
-        return html.P('*Link not found', id='tempP', style={'color': '#cc0000', 'position': 'relative', 'bottom': '300px'})
+def handleDeleteBranch(n_clicks, node1, node2):
+
+    if not node1:
+        return html.P('*Please enter a value for Node 1', id='tempP', style={'color': '#cc0000', 'position': 'relative', 'bottom': '300px'})
+    elif node1 and node2:
+        return html.P('*Connection between node1 and node2 deleted.', id='tempP', style={'color': '#49af41', 'position': 'relative', 'bottom': '300px'})
     else:
-        return html.P('*Link was deleted successfully', id='tempP', style={'color': '#49af41', 'position': 'relative', 'bottom': '300px'})
-
-@callback(
-    Output('hidden_div_for_redirect_callback_delete_node', 'children'),
-    Input('deleteNodeButton', 'n_clicks'),
-    State('nodeTitle', 'value'),
-    prevent_initial_call=True
-)
-
-def handleDeleteNode(n_clicks, node):
-	print((link))
-	if not link:
-		return html.P('*Node not found', id='tempP', style={'color': '#cc0000', 'position': 'relative', 'bottom': '300px'})
-	else:
-		return html.P('*Node was deleted successfully', id='tempP', style={'color': '#49af41', 'position': 'relative', 'bottom': '300px'})
+        return html.P('*Node 1 was deleted successfully', id='tempP', style={'color': '#49af41', 'position': 'relative', 'bottom': '300px'})
 
 @callback(
 	Output('hidden_div_for_redirect_callback_return_home', 'children'),
