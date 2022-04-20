0a9daf8094c606156e5812a111ebf69fec0f3d32
Abram Munoz <abrammunoz@rocketmail.com>
2022-04-19 22:11:27 -0500

    Adding to the Database, as an error viewing 3D graph

--- src/pages/AddNewBranch.py
+++ src/pages/AddNewBranch.py
@@ -1,35 +1,82 @@
 from dash import Dash, dcc, html, Input, Output, callback, State
+import mysql.connector
+
+#Connect to database and create a cursor to interact with database
+bayerdb = mysql.connector.connect(user='root', password='root', host='localhost', database='bayerdatabase')
+cursor = bayerdb.cursor(buffered=True)
+
+branch_levels = []
+
+select_all_branch_levels = "SELECT branch_level FROM org_chart_branches"
+
+cursor.execute(select_all_branch_levels)
+
+next_level = cursor.fetchone()
+
+while next_level is not None:
+	
+    next_branch_level_string = str(''.join(map(str, next_level)))
+
+    branch_levels.append(next_branch_level_string)
+
+    next_level = cursor.fetchone()
+#removes duplicates for drop down menu
+branch_levels = list(set(branch_levels))
+print(branch_levels)
+
 
 layout = html.Div(className='AddNewBranchMain', 
 	children=[
-		html.H1('Add New Branch'),
-		html.Div(className='AddBranchBody', 
+		html.H1('Add New Branch', style={'color':'white'}),
+
+		html.Div(className='AddLevel', 
 			children=[
-				html.P('Branch name:', id='branchNameP', className='paragraphCredential'),
-				dcc.Input(id='branchName', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
-				html.P(id='spacing'),
-				html.Button('Add branch', id='addBranchButton', className='addBranchCss')
+				#html.P("Branch 1: ", id="Node1", className="node1Label"),
+                dcc.Dropdown(id="branchLevel", options=branch_levels, placeholder="Select the level of the Branch"),
+                html.P(id='spacing'),
+				dcc.Input(id='branchName', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}, placeholder="Enter a Branch Name"),
+				html.Br(),
+				dcc.Input(id='numEmployees', type='number', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}, placeholder="Enter Number of Employees"),
+				html.Br(),
+				dcc.Input(id='branchDescription', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}, placeholder="Enter a Branch Description"),
 			],
 			style={'paddingTop': '90px'}
 		),
+
+
+		html.Button('Add Branch', id='addButton', className='addButtonCss', style={'position': 'relative', 'top': '180px'}),
+		html.Br(),
 		html.Button('Home', id='homeButton', className='homeButtonCss', style={'position': 'relative', 'top': '180px'}),
 		html.Div(id='hidden_div_for_redirect_callback_add_branch'),
 		html.Div(id='hidden_div_for_redirect_callback_home_button')
 	]
 )
 
+
+
 @callback(
 	Output('hidden_div_for_redirect_callback_add_branch', 'children'),
-	Input('addBranchButton', 'n_clicks'),
+	Input('addButton', 'n_clicks'),
+	State('branchLevel', 'value'),
 	State('branchName', 'value'),
+	State('numEmployees', 'value'),
+	State('branchDescription', 'value'),
 	prevent_initial_call=True
 )
-def handleAddBranch(n_clicks, branch):
-	print((branch))
-	if not branch:
+def handleAddBranch(n_clicks, level, branch, num, descriptions):
+	
+	if not level or not branch or not num or not descriptions:
 		return html.P('* Invalid branch name', id='tempP', style={'color': '#cc0000', 'position': 'relative', 'bottom': '300px'})
 	else:
-		return html.P('* Branch was added successfully', id='tempP', style={'color': '#49af41', 'position': 'relative', 'bottom': '300px'})
+		cursor.execute(
+				"""INSERT INTO org_chart_branches 
+				(branch_level, branch_title, num_of_employees, branch_description) 
+				VALUES(%s, %s, %s, %s)""", (level, branch, num, descriptions))	
+				
+		bayerdb.commit()
+
+
+		return html.P(level + ' ' + branch + ' ' + str(num) + ' ' + descriptions + ' * Branch was added successfully', id='tempP', style={'color': 'white', 'position': 'relative', 'bottom': '80px'})
 
 @callback(
 	Output('hidden_div_for_redirect_callback_home_button', 'children'),
