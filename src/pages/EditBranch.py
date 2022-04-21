d0ef668489eb6ba8fd3f99d12ac78984ac8ee94c
Abram Munoz <abrammunoz@rocketmail.com>
2022-04-20 23:33:49 -0500

    can change the name of edit file

--- src/pages/EditBranch.py
+++ src/pages/EditBranch.py
@@ -1,31 +1,51 @@
 from dash import Dash, dcc, html, Input, Output, callback, State
+import mysql.connector
+
+
+#Connect to database and create a cursor to interact with database
+bayerdb = mysql.connector.connect(user='root', password='root', host='localhost', database='bayerdatabase')
+cursor = bayerdb.cursor(buffered=True)
+
+
+branch_titles = []
+
+select_all_branch_titles = "SELECT branch_title FROM org_chart_branches"
+
+cursor.execute(select_all_branch_titles)
+
+next_title = cursor.fetchone()
+
+while next_title is not None:
+
+    next_branch_title_string = str(''.join(map(str, next_title)))
+
+    branch_titles.append(next_branch_title_string)
+
+    next_title = cursor.fetchone()
+
+
 
 layout = html.Div(className='EditBranchMain', 
 	children=[
       html.Div(className='AddBranchBody', 
 			children=[
 				html.H1('Edit a Branch'),
-				html.P('Select Branch:', id='branchNameP', className='paragraphCredential'),
-				dcc.Input(id='branchName', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
-				html.P('New info 1:', id='branchNameP1', className='paragraphCredential'),
-				dcc.Input(id='info1', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
-                html.P('New info 2:', id='branchNameP2', className='paragraphCredential'),
-				dcc.Input(id='info2', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
-                html.P('New info 3:', id='branchNameP3', className='paragraphCredential'),
-				dcc.Input(id='info3', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '20px'}),
-                html.P(id='spacing'),
+				
+				dcc.Dropdown(id="branch", options=branch_titles, placeholder="Select a branch to edit", multi=True),
+				html.Br(),
+				dcc.Input(id='branchName', type='text', style={'marginTop': '50px', 'margin': '-10px', 'width': '50%', 'borderRadius': '7px', 'border': '1px solid grey', 'height': '35px'}, placeholder="Enter a Branch Name"),
+				html.Br(),
+				
 				html.Button('Edit', id='editBranchButton', className='addBranchCss'),
+				html.Button('Home', id='homeButton', className='homeButtonCss', style={'position': 'relative'}),
 				
 
-                html.P('Current Info 1: xxxxxx', id='branchNameP4', className='paragraphCredential'),
-                html.P('Current Info 2: xxxxxx', id='branchNameP5', className='paragraphCredential'),
-                html.P('Current Info 3: xxxxxx', id='branchNameP6', className='paragraphCredential'),
-				html.Br(),
+                html.Br(),
 				html.Img(className='bayerButton', src="/assets/img/bayer.png" , style={'height':'10%', 'width':'10%', 'left': '20px'}),
 			],
 			style={'paddingTop': '90px'}
 		),
-        html.Button('Home', id='homeButton', className='homeButtonCss', style={'position': 'relative', 'top': '180px'}),
+        
 		html.Div(id='hidden_div_for_redirect_callback_edit_branch'),
 		html.Div(id='hidden_div_for_redirect_callback_home_buttonEdit')
     ]
@@ -34,18 +54,23 @@ layout = html.Div(className='EditBranchMain',
 @callback(
 	Output('hidden_div_for_redirect_callback_edit_branch', 'children'),
 	Input('editBranchButton', 'n_clicks'),
+	State('branch', 'value'),
 	State('branchName', 'value'),
-	State('info1', 'value'),
-	State('info2', 'value'),
-	State('info3', 'value'),
 	prevent_initial_call=True
 )
-def handleEdit(n_clicks, branchName, info1, info2, info3):
-	if not branchName or not info1 or not info2 or not info3:
+def handleEdit(n_clicks, branch, branchName):
+	if not branch or not branchName:
 		return html.P(("Empty Fields"), id="tempPEdit")
 
 	else:
-		return html.P(("For branch: " + branchName + " - change: " + info1 + ", " + info2 + ", " + info3), id="tempPEdit")
+		str1 = str(''.join(branch))
+		cursor.execute(
+				"""UPDATE org_chart_branches 
+				SET branch_title=%s
+				WHERE branch_title=%s""", (branchName, str1))
+				
+		bayerdb.commit()
+		return html.P(("For branch: " + str(branch)+ " - change: " + branchName), id="tempPEdit")
 
 
 @callback(
